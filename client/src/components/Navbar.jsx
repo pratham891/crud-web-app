5b7ea2fdbf2e38c5d2ebff14b3397b0962a28816
Pratham Singh <prathambsingh891@gmail.com>
2024-01-15 23:51:49 +0530

    Revert "update navbar"
    This reverts commit ac609464432d8385c6f1edbde5e0125f14b4c9fb.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,6 +9,13 @@ const Navbar = () => {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+                    <form className="d-flex" role="search">
+                        {/* <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" /> */}
+                        <button className="btn btn-outline-success" type="submit">Register</button>
+                    </form>
+                </div>
             </div>
         </nav>
     )
