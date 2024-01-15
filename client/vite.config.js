d66de659100809d479cc242b0e9e888f945c0f68
Pratham Singh <88388286+pratham891@users.noreply.github.com>
2024-01-15 23:48:42 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3003',
-        changeOrigin: true,
-      },
-    }
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:3003',
+  //       changeOrigin: true,
+  //     },
+  //   }
+  // },
   plugins: [react()],
 })
