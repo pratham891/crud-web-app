5b73fa157e23d7be82b16a086d246a75df2c5785
Pratham Singh <88388286+pratham891@users.noreply.github.com>
2024-01-15 23:46:39 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use(cors(
     {
-        origin:[""],
+        origin:["https://crud-web-app-nine.vercel.app/"],
         methods: ["POST", "GET", "PUT", "DELETE"],
         credentials: true
     }
