2f25752e28b147a4b503ac9831baef0d60434478
Pratham Singh <prathambsingh891@gmail.com>
2024-08-08 01:37:13 +0530

    add host server proxy api target

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3003',
+        target: 'https://crud-web-app-server.vercel.app',
         rewrite: (path) => path.replace(/^\/api/, ''),
         changeOrigin: true
       },
